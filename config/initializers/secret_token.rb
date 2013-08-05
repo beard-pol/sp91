9c35765e798afd473885bf30c7f0541a8fa4d05e
Andrey Valkovsky <vars90@mail.ru>
2013-08-05 23:36:17 +0400

    Добавлен каркас проекта

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sp91::Application.config.secret_key_base = 'bf66246807edad691c8b517f6317bbfeaca058039636edafc4f17117bee0f5b1c5d35d788a5f095ae7a06c08e4534105cd4814a9fba159f4ca7c6a0c68231b86'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sp91::Application.config.session_store :cookie_store, key: '_sp91_session'
